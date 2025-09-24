db7553bdd1f2e0189daca63cf78c31ed325484d7
Dan Slaughter <dan@brightspot.com>
2025-09-24 13:07:44 -0400

    Add RestaurantEndpoint GraphQL endpoint.
    Introduces RestaurantEndpoint class extending GCAEndpoint and implementing Singleton. Sets up a GraphQL endpoint at /gca with default schema settings.

---
+++ core/src/main/java/com/brightspot/example/restaurant/RestaurantEndpoint.java
@@ -0,0 +1,21 @@
+package com.brightspot.example.restaurant;
+
+import java.util.Set;
+
+import com.psddev.dari.db.Singleton;
+import com.psddev.graphql.gca.GCAEndpoint;
+import com.psddev.graphql.gca.GCASchemaSettings;
+
+public class RestaurantEndpoint extends GCAEndpoint implements Singleton {
+
+    @Override
+    public Set<String> getPaths() {
+        return Set.of("/gca");
+    }
+
+    @Override
+    protected GCASchemaSettings getSchemaSettings() {
+        return GCASchemaSettings.newBuilder()
+            .build();
+    }
+}
